35fb1acb106e9e06b732826920e9b00801f0221a
Raj Kr. Soni <72248764+bugraj@users.noreply.github.com>
2024-08-03 23:47:59 +0530

    Time: 303 ms (27.27%), Space: 181.7 MB (45.45%) - LeetHub

---
+++ 3239-minimum-number-of-flips-to-make-binary-grid-palindromic-i/3239-minimum-number-of-flips-to-make-binary-grid-palindromic-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minFlips(vector<vector<int>>& grid) {
+        int n = grid.size(), m=grid[0].size();
+        int row=0,col=0;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m/2;j++)
+            {
+                if(grid[i][j]!= grid[i][m-j-1]) row++;
+            }
+        }
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n/2;j++)
+            {
+                if(grid[j][i]!=grid[n-j-1][i]) col++;
+            }
+        }
+        return min(row,col);
+        
+    }
+};
